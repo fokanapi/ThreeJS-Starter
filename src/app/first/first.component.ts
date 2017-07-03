6ee147e50e3aefa493df1422b0a1b0abb853bfcf
Francisco Kanapi <fokanapi@gmail.com>
2017-07-03 19:26:30 +0800

    box done

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -25,28 +25,30 @@ export class FirstComponent implements AfterViewInit {
     self.mesh.rotation.y += 0.02;
 
     self.renderer.render(self.scene, self.camera);
-
+    
   }
 
   ngAfterViewInit() {
     this.init();
     this.animate();
+    console.log(this.scene);
   }
 
 
   init() {
     let self = this;
     self.scene = new THREE.Scene();
-
-    self.camera = new THREE.PerspectiveCamera(100, (window.innerWidth/2) / window.innerHeight, 1, 1000);
-    self.camera.position.z = 500;
-    self.geometry = new THREE.BoxGeometry(100, 30, 200);
+    //Camera perpective usually 35-45
+    self.camera = new THREE.PerspectiveCamera(45, (window.innerWidth/2) / window.innerHeight, 1, 1000);
+    self.camera.position.z = 100;
+    self.geometry = new THREE.BoxGeometry(20, 20, 20);
     self.material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 
     self.mesh = new THREE.Mesh(self.geometry, self.material);
     self.scene.add(self.mesh);
 
-    self.renderer = new THREE.WebGLRenderer();
+    self.renderer = WebGLRenderingContext ? new THREE.WebGLRenderer(): new THREE.CanvasRenderer();
+  
     self.renderer.setSize(window.innerWidth/2, window.innerHeight);
 
     document.body.appendChild(self.renderer.domElement);
